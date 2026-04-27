af528a5f8601f4a75a9ec6ded558168e9e086282
Oscar Garrido <osca.garrido@duocuc.cl>
2026-04-27 15:12:40 -0400

    feat: add list command

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const { readTasks } = require('./tasks');
+// Agrega esto en index.js
+const { readTasks, writeTasks } = require('./tasks');
 const [,, cmd] = process.argv;
 
-if (!cmd) {
-    console.log('TastMaster CLI - usa: node index.js <comando>');
+if (cmd === 'list') {
+  const tasks = readTasks();
+  if (tasks.length === 0) {
+    console.log('No hay tareas.');
+  } else {
+    tasks.forEach((t, i) => {
+      const icon = t.done ? '✓' : '○';
+      console.log(`${icon} [${i + 1}] ${t.title}`);
+    });
+  }
 }
